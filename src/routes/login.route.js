11238b1cf61a41f791b202f3594006c5881f699c
RobertoQuijano <robertoemmanuel.reqmnmail.com>
2024-04-16 10:23:06 -0600

    Actualizacion

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const usuarioController = require('./usuarioController');
+const usuarioController = require('../controller/login.controller');
 
 // Rutas
 router.get('/', usuarioController.getUsuarios);
