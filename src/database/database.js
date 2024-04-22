987486c66803a6e3003157bfd12736879d70916a
Roberto Quijano <48535848+Quijano132@users.noreply.github.com>
2024-04-22 09:41:50 -0600

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize';
-2const sequelize = new Sequelize('b0hqlhkhmg0xm49clruh', 'uaxytqzwqmyzm6ir', 'ONSomhSkiSzjZ0h5ACWX', {
+
+const sequelize = new Sequelize('b0hqlhkhmg0xm49clruh', 'uaxytqzwqmyzm6ir', 'ONSomhSkiSzjZ0h5ACWX', {
   host: 'b0hqlhkhmg0xm49clruh-mysql.services.clever-cloud.com',
   dialect: 'mysql',
   connectTimeout: 30000,
