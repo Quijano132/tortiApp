ae57d7279772191543acd72782d73e4fbe458b64
Roberto Quijano <48535848+Quijano132@users.noreply.github.com>
2024-04-23 15:29:45 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -25,6 +25,12 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 
 app.get('/', (req, res )=>{
